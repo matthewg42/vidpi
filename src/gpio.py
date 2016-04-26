4c4d911c6745d3714400974abe2a7384937f278a
Matthew Gates <matthewg42@gmail.com>
2016-04-26 10:25:32 +0100

    detect and display times/duration

--- src/gpio.py
+++ src/gpio.py
@@ -2,21 +2,33 @@
 
 import RPi.GPIO as GPIO
 import time
+import logging
+
+log = logging
+log.basicConfig(format='%(asctime)-15s %(message)s', level=logging.INFO)
 
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 4
 GPIO.setup(PIR_PIN, GPIO.IN)
+started = None
 
 try:
-        print "PIR Module Test (CTRL+C to exit)"
-        time.sleep(2)
-        print "Ready"
-        while True:
-                if GPIO.input(PIR_PIN):
-                        print "Motion Detected!"
-                time.sleep(1)
+    print "PIR Module Test (CTRL+C to exit)"
+    while True:
+        if GPIO.input(PIR_PIN):
+            if started is None:
+                started = time.time()
+                log.info('motion: STARED')
+            else:
+                log.debug('motion: CONTINUED')
+        else:
+            if started is not None:
+                log.info('motion: ENDED, duration was %.1f' % (time.time() - started))
+                started = None
+            else:
+                log.debug('motion: nope')
+        time.sleep(0.1)
 except KeyboardInterrupt:
-        print " Quit"
-        GPIO.cleanup()
-
+    print " Quit"
+    GPIO.cleanup()
 
